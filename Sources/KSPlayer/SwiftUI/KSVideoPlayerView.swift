882279c124fb28cdeac38fd3b8220280cd84c834
Maniganda Saravanan <rsaranit@gmail.com>
2025-07-21 19:07:31 +0400

    s

--- Sources/KSPlayer/SwiftUI/KSVideoPlayerView.swift
+++ Sources/KSPlayer/SwiftUI/KSVideoPlayerView.swift
@@ -477,7 +477,7 @@ struct VideoControllerView: View {
                             Text("🌐︎ Download subtitle")
                                 .font(.caption)
                                 .foregroundColor(isDowloadSubtitleFocused ? .black : .white)
-//                                .padding(8)
+                                .padding(8)
                         }
                         .focused($isDowloadSubtitleFocused)
                         .frame(width: 320)
